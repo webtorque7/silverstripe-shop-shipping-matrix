2ef9c7ba6ec49fc1d7b8ba064b3df793bbaad461
Conrad <conrad@webtorque.co.nz>
2014-11-26 16:31:50 +1300

    make required, fix up value calculating using order information

--- code/ShippingMatrixModifier.php
+++ code/ShippingMatrixModifier.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: davis
  * Date: 06/11/13
  * Time: 13:58
  */
-
 class ShippingMatrixModifier extends ShippingModifier
 {
 	private static $db = array(
@@ -14,9 +14,9 @@ class ShippingMatrixModifier extends ShippingModifier
 	);
 
 	public function populate($data = null) {
-		if(!empty($data)){
+		if (!empty($data)) {
 			$shippingCharge = 0;
-			if ($deliveryCountry = $data['DeliveryCountry']){
+			if ($deliveryCountry = $data['DeliveryCountry']) {
 				$this->DefaultCountry = $deliveryCountry;
 			}
 			//TODO currently region field doesn't exist on checkout page.
@@ -37,8 +37,7 @@ class ShippingMatrixModifier extends ShippingModifier
 			}
 			$this->Amount = $shippingCharge;
 			$this->write();
-		}
-		else{
+		} else {
 			$this->ShippingType = null;
 			$this->Amount = 0;
 			$this->write();
@@ -50,8 +49,24 @@ class ShippingMatrixModifier extends ShippingModifier
 	}
 
 	public function value($subtotal = null) {
-		//Shipping modifier is calculated using the populate function.
-		return 0;
+		$this->ShippingType = $this->Order()->OrderShippingType;
+		$shippingCharge = 0;
+
+		if ($this->ShippingType !== 'free' && $this->ShippingType !== 'pickup') {
+			$address = $this->Order()->ShippingAddress();
+
+			if ($address->exists()) {
+				$items = $this->Order()->Items();
+
+				if ($address->Country === SiteConfig::current_site_config()->DomesticCountry) {
+					$shippingCharge = DomesticShippingCarrier::process($items, $address->Region);
+				} else {
+					$shippingCharge = InternationalShippingCarrier::process($items, $address->Country);
+				}
+			}
+		}
+
+		return $shippingCharge;
 	}
 
 	public function TableTitle() {
@@ -64,13 +79,12 @@ class ShippingMatrixModifier extends ShippingModifier
 
 		if (!($countries = $cache->load('deliveryCountry'))) {
 			$defultZone = InternationalShippingZone::get()->filter('DefaultZone', true)->first();
-			if(!empty($defultZone)){
+			if (!empty($defultZone)) {
 				SiteConfig::current_site_config()->getCountriesList();
-			}
-			else{
+			} else {
 				$countries = array();
 				$zones = InternationalShippingZone::get();
-				foreach($zones as $zone){
+				foreach ($zones as $zone) {
 					array_push($countries, $zone->ShippingCountries);
 				}
 				asort($countries);
