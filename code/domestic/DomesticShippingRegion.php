82a4e14374c5369d425ea122a880c267b63a3eef
Carey <carey@webtorque.co.nz>
2016-05-20 14:41:33 +1200

    sparate out the part where it returns all regions for the warehouse country, this will be used by all region getters

--- code/domestic/DomesticShippingRegion.php
+++ code/domestic/DomesticShippingRegion.php
@@ -1,64 +1,70 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: User
  * Date: 9/05/14
  * Time: 1:28 PM
  */
+class DomesticShippingRegion extends DataObject
+{
+    private static $db = array(
+        'Title' => 'Varchar(100)',
+        'Region' => 'Text',
+        'Sort' => 'Int',
+        'Amount' => 'Currency',
+        'DefaultRegion' => 'Boolean'
+    );
 
-class DomesticShippingRegion extends DataObject{
-	private static $db = array(
-		'Title' => 'Varchar(100)',
-		'Region' => 'Text',
-		'Sort' => 'Int',
-		'Amount' => 'Currency',
-		'DefaultRegion' => 'Boolean'
-	);
+    private static $has_one = array(
+        'DomesticShippingCarrier' => 'DomesticShippingCarrier'
+    );
 
-	private static $has_one = array(
-		'DomesticShippingCarrier' => 'DomesticShippingCarrier'
-	);
+    private static $summary_fields = array(
+        'Title' => 'Title',
+        'Region' => 'Region',
+        'Amount' => 'Amount'
+    );
 
-	private static $summary_fields = array(
-		'Title' => 'Title',
-		'Region' => 'Region',
-		'Amount' => 'Amount'
-	);
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(array(
+            'Sort',
+            'DomesticShippingCarrierID'
+        ));
 
-	public function getCMSFields(){
-		$fields = parent::getCMSFields();
-		$fields->removeByName(array(
-			'Sort',
-			'DomesticShippingCarrierID'
-		));
+        $fields->addFieldsToTab('Root.Main', array(
+            TextField::create('Title', 'Title'),
+            TextField::create('Region', 'Region'),
+            TextField::create('Amount', 'Amount'),
+            CheckboxField::create('DefaultRegion', 'Default Region'),
+        ));
 
-		$region = singleton('Address')->getRegionList();
+        return $fields;
+    }
 
-		$fields->addFieldsToTab('Root.Main', array(
-			TextField::create('Title', 'Title'),
-			CheckboxField::create('DefaultRegion', 'Default Region'),
-			CheckboxSetField::create('Region', 'Region', $region),
-			TextField::create('Amount', 'Amount')
-		));
+    public static function get_shipping_region($deliveryRegion)
+    {
+        $availableRegions = self::get_available_regions();
+        if (!empty($availableRegions)) {
+            $shippingRegion = $availableRegions->filter('Region:PartialMatch', $deliveryRegion)->first();
 
-		return $fields;
-	}
-	public static function get_shipping_region($deliveryRegion)
-	{
-		$shippingMatrix = ShippingMatrixConfig::current();
-		$domesticCarriers = $shippingMatrix->DomesticShippingCarriers();
-		$carrierIDs = implode(',', $domesticCarriers->column('ID'));
+            if ($shippingRegion && $shippingRegion->exists()) {
+                return $shippingRegion;
+            }
 
-		$shippingRegion = DomesticShippingRegion::get()
-				->filter('Region:PartialMatch', $deliveryRegion)
-				->where('"DomesticShippingCarrierID" IN (' . $carrierIDs . ')')
-				->first();
+            //fall back looking for default region
+            return $availableRegions->filter(array('DefaultRegion', true))->first();
+        }
+    }
 
-		if($shippingRegion && $shippingRegion->exists()){
-			return $shippingRegion;
-		}
+    public static function get_available_regions()
+    {
+        $shippingMatrix = ShippingMatrixConfig::current();
+        $domesticCarriers = $shippingMatrix->DomesticShippingCarriers();
+        $carrierIDs = implode(',', $domesticCarriers->column('ID'));
 
-		//fall back looking for default region
-		return DomesticShippingRegion::get()->filter(array('DefaultRegion', true))->first();
-	}
+        return DomesticShippingRegion::get()->where('"DomesticShippingCarrierID" IN (' . $carrierIDs . ')');
+    }
 }
